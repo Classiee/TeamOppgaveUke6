7885c325816be69908dcc243388cbf337eb2a973
Classiee <Kristianfratini@gmail.com>
2024-09-10 13:33:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -42,20 +42,20 @@ function Kompis() {
 
     this.hils = function (hilsen) {
         if (hilsen === this.riktigHilsen) {
-            if (bil.kulhet > 70) {
-                // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
-                bil.kulhet += 10;
+            if (kulhetsFaktor > 70) {
+                kulhetsFaktor += 10;
+                updateImg = "IMG\carkompis1upgrade2.png"
             } else {
                 alert("Greit nok, men du kan bli mye kulere!");
-                bil.kulhet -= 5;
+                kulhetsFaktor -= 5;
             }
         } else {
             alert("Du hilste feil, og bilen din er ikke kul!");
-            bil.kulhet -= 10;
-            if (bil.kulhet > 70) {
+            kulhetsFaktor -= 10;
+            if (kulhetsFaktor > 70) {
                 alert("Du er fortsatt kul, men dette ble en klein situasjon.");
             } else {
-                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
+                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor.");
             }
         }
         skjulHilseValg();
@@ -68,20 +68,20 @@ function Kompis1() {
 
     this.hils = function (hilsen) {
         if (hilsen === this.riktigHilsen) {
-            if (bil.kulhet > 70) {
-                // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
-                bil.kulhet += 10;
+            if (kulhetsFaktor > 70) {
+                
+                kulhetsFaktor += 10;
             } else {
                 alert("Greit nok, men du kan bli mye kulere!");
-                bil.kulhet -= 5;
+                kulhetsFaktor -= 5;
             }
         } else {
             alert("Du hilste feil, og bilen din er ikke kul!");
-            bil.kulhet -= 10;
-            if (bil.kulhet > 70) {
+            kulhetsFaktor -= 10;
+            if (kulhetsFaktor > 70) {
                 alert("Du er fortsatt kul, men dette ble en klein situasjon.");
             } else {
-                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
+                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor.");
             }
         }
         skjulHilseValg();
@@ -94,20 +94,20 @@ function Kompis2() {
 
     this.hils = function (hilsen) {
         if (hilsen === this.riktigHilsen) {
-            if (bil.kulhet > 70) {
-                // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
-                bil.kulhet += 10;
+            if (kulhetsFaktor > 70) {
+                
+                kulhetsFaktor += 10;
             } else {
                 alert("Greit nok, men du kan bli mye kulere!");
-                bil.kulhet -= 5;
+                kulhetsFaktor -= 5;
             }
         } else {
             alert("Du hilste feil, og bilen din er ikke kul!");
-            bil.kulhet -= 10;
-            if (bil.kulhet > 70) {
+            kulhetsFaktor -= 10;
+            if (kulhetsFaktor > 70) {
                 alert("Du er fortsatt kul, men dette ble en klein situasjon.");
             } else {
-                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
+                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor.");
             }
         }
         skjulHilseValg();
