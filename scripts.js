e7c4a5a3b6334674d1c5618b9ce392eabf426381
Classiee <Kristianfratini@gmail.com>
2024-09-10 12:38:02 +0200

    Barebone V1

---
+++ scripts.js
@@ -0,0 +1,46 @@
+let bil = 
+
+function Kompis(){
+    
+}
+
+function Kompis1(){
+    
+}
+
+function Kompis2(){
+    
+}
+
+function jehovasVitne(){
+    
+}
+
+function bestemor(){
+    
+}
+
+function flatEarther(){
+    
+}
+
+function upgrade(){
+    
+}
+
+function hilse(){
+    
+}
+
+function randomEvent(){
+    
+}
+
+function kjør(){
+    
+}
+
+function skjulHilseValg(){
+    
+}
+
