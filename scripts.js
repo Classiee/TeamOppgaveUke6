05bfa0a3c69be5a637d051af8d5b724e1cd6302a
Classiee <Kristianfratini@gmail.com>
2024-09-10 13:19:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,87 +38,83 @@ function visAlt() {
         `;
     }
 
-function Kompis() {
-    const hilsninger = ["high five", "fist bump", "nikk"];
-    this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
-
-    this.hils = function (hilsen) {
-        if (hilsen === this.riktigHilsen) {
-            if (bil.kulhet > 70) {
-                // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
-                bil.kulhet += 10;
-
-            }
-        } else {
-            alert("Greit nok, men du kan bli mye kulere!");
-            bil.kulhet -= 5;
-        }
-    } else {
-        alert("Du hilste feil, og bilen din er ikke kul!");
-        bil.kulhet -= 10;
-        if (bil.kulhet > 70) {
-            alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-        } else {
-            alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
-        }
-    }
-    skjulHilseValg();
-}
-
-function Kompis1() {
-
-    const hilsninger = ["high five", "fist bump", "nikk"];
-    this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
+    function Kompis() {
+        const hilsninger = ["high five", "fist bump", "nikk"];
+        this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
     
-    this.hils = function(hilsen) {
-        if (hilsen === this.riktigHilsen) {
-            if (bil.kulhet > 70) {
-                // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
-                bil.kulhet += 10;
-                
+        this.hils = function (hilsen) {
+            if (hilsen === this.riktigHilsen) {
+                if (bil.kulhet > 70) {
+                    // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
+                    bil.kulhet += 10;
+                } else {
+                    alert("Greit nok, men du kan bli mye kulere!");
+                    bil.kulhet -= 5;
+                }
+            } else {
+                alert("Du hilste feil, og bilen din er ikke kul!");
+                bil.kulhet -= 10;
+                if (bil.kulhet > 70) {
+                    alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+                } else {
+                    alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
+                }
             }
-        } else {
-            alert("Greit nok, men du kan bli mye kulere!");
-            bil.kulhet -= 5;
-        }
-    } else {
-        alert("Du hilste feil, og bilen din er ikke kul!");
-        bil.kulhet -= 10;
-        if (bil.kulhet > 70) {
-            alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-        } else {
-            alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
-        }
+            skjulHilseValg();
+        };
     }
-    skjulHilseValg();
-}
-
-function Kompis2(){
-    const hilsninger = ["high five", "fist bump", "nikk"];
-    this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
     
-    this.hils = function(hilsen) {
-        if (hilsen === this.riktigHilsen) {
-            if (bil.kulhet > 70) {
-                // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
-                bil.kulhet += 10;
-                
+    function Kompis1() {
+        const hilsninger = ["high five", "fist bump", "nikk"];
+        this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
+    
+        this.hils = function (hilsen) {
+            if (hilsen === this.riktigHilsen) {
+                if (bil.kulhet > 70) {
+                    // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
+                    bil.kulhet += 10;
+                } else {
+                    alert("Greit nok, men du kan bli mye kulere!");
+                    bil.kulhet -= 5;
+                }
+            } else {
+                alert("Du hilste feil, og bilen din er ikke kul!");
+                bil.kulhet -= 10;
+                if (bil.kulhet > 70) {
+                    alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+                } else {
+                    alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
+                }
             }
-        } else {
-            alert("Greit nok, men du kan bli mye kulere!");
-            bil.kulhet -= 5;
-        }
-    } else {
-        alert("Du hilste feil, og bilen din er ikke kul!");
-        bil.kulhet -= 10;
-        if (bil.kulhet > 70) {
-            alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-        } else {
-            alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
-        }
+            skjulHilseValg();
+        };
     }
-    skjulHilseValg();
-}
+    
+    function Kompis2() {
+        const hilsninger = ["high five", "fist bump", "nikk"];
+        this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
+    
+        this.hils = function (hilsen) {
+            if (hilsen === this.riktigHilsen) {
+                if (bil.kulhet > 70) {
+                    // getElementById("result").innerHTML = "Du hilste riktig, og bilen din er blitt kul!";
+                    bil.kulhet += 10;
+                } else {
+                    alert("Greit nok, men du kan bli mye kulere!");
+                    bil.kulhet -= 5;
+                }
+            } else {
+                alert("Du hilste feil, og bilen din er ikke kul!");
+                bil.kulhet -= 10;
+                if (bil.kulhet > 70) {
+                    alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+                } else {
+                    alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
+                }
+            }
+            skjulHilseValg();
+        };
+    }
 
 function jehovasVitne(){
 
