b8977fbe2cacc486adc3fbf8043e7007f3971b9a
Classiee <Kristianfratini@gmail.com>
2024-09-11 13:01:25 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 let bil;
 let kulhetsFaktor = 0;
 let updateImg;
@@ -8,70 +7,70 @@ let exitText;
 
 updateView();
 function updateView() {
-    document.getElementById('app').innerHTML = /*HTML*/`
+  document.getElementById("app").innerHTML = /*HTML*/ `
     <div>
         <div class="progressBar">
             <div style="width: ${kulhetsFaktor}%" class="updateBar"><strong>${kulhetsFaktor}%</strong></div>
         </div>
         <div class="text">😎</div>
-            <img class="image" src="${updateImg ?? 'IMG/carempty.jpg'}"/>
-            ${kompisDialog ?? ''}
-            ${bilDialog ?? ''}
-            ${exitText ?? ''}
+            <img class="image" src="${updateImg ?? "IMG/carempty.jpg"}"/>
+            ${kompisDialog ?? ""}
+            ${bilDialog ?? ""}
+            ${exitText ?? ""}
         </div>
     `;
-        if (kulhetsFaktor >= 100) {
-            gameWon();
-        } else if (kulhetsFaktor < 0) {
-            gameOver();
-        }
+  if (kulhetsFaktor >= 100) {
+    gameWon();
+  } else if (kulhetsFaktor < 0) {
+    gameOver();
+  }
 }
-function gameOver(){
-    exitText = `
+function gameOver() {
+  exitText = `
     <div class="gameOver">Game Over!
     <button class="gameOverBtn" onclick="location.reload()">Try again</button></div>`;
-    kompisDialog = '';
-    bilDialog = '';
-    updateView();
+  kompisDialog = "";
+  bilDialog = "";
+  updateView();
 }
-function gameWon(){
-    exitText = `
+function gameWon() {
+  exitText = `
         <div class="gameOver">DU VANT!
         <button class="gameOverBtn" onclick="location.reload()">Try again</button></div>`;
-    kompisDialog = '';
-    bilDialog = '';
-    updateView();
+  kompisDialog = "";
+  bilDialog = "";
+  updateView();
 }
 
 function start() {
-    exitText = `<div class="gameOver">Ready?
+  exitText = `<div class="gameOver">Ready?
             <button class="gameOverBtn" onclick="placeHolder()">Start</button></div>`;
-    kompisDialog = '';
-    bilDialog = '';
-        updateView();
+  kompisDialog = "";
+  bilDialog = "";
+  updateView();
 }
-function placeHolder(){
-    if (kulhetsFaktor <= 30) {
-        updateImg = 'IMG/caroil.jpg';
-    } else if (kulhetsFaktor >= 30 && kulhetsFaktor <= 60) {
-        updateImg = 'IMG/caroilupgrade1.jpg';
-    } else if (kulhetsFaktor >= 60 && kulhetsFaktor <= 100) {
-        updateImg = 'IMG/caroilupgrade2.jpg';
-    }
-    exitText = '';
-    bilDialog = `
+function placeHolder() {
+  if (kulhetsFaktor <= 30) {
+    updateImg = "IMG/caroil.jpg";
+  } else if (kulhetsFaktor >= 30 && kulhetsFaktor <= 60) {
+    updateImg = "IMG/caroilupgrade1.jpg";
+  } else if (kulhetsFaktor >= 60 && kulhetsFaktor <= 100) {
+    updateImg = "IMG/caroilupgrade2.jpg";
+  }
+  exitText = "";
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "">Upgrades? Tja...</button>
             <button onclick = "">why, kjerra er jo grom?</button>
         </div>`;
-    kompisDialog = `
+  kompisDialog = `
         <div class="kompisDialog">Vil du ha en Upgrade?
             </div>`;
-            updateView();
+  updateView();
 }
 
 function Kompis() {
-    bilDialog = `
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "hilse()">👋</button>
             <button onclick = "hilse()">👊</button>
@@ -79,11 +78,11 @@ function Kompis() {
         </div>`;
   const hilsninger = ["high five", "fist bump", "nikk"];
   this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
-  
+
   if (kulhetsFaktor <= 30) {
-    updateImg ="IMG/carkompis1.png";
+    updateImg = "IMG/carkompis1.png";
   } else if (kulhetsFaktor >= 30 && kulhetsFaktor <= 60) {
-    updateImg ="IMG/carkompis1upgrade1.png";
+    updateImg = "IMG/carkompis1upgrade1.png";
   } else if (kulhetsFaktor >= 60 && kulhetsFaktor <= 100) {
     updateImg = "IMG/carkompis1upgrade2.png";
   }
@@ -101,18 +100,18 @@ function Kompis() {
       alert("Du hilste feil, og bilen din er ikke kul!");
       kulhetsFaktor -= 10;
       if (kulhetsFaktor < 70) {
-            alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-        } else {
-            alert(
-             "Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor."
-            );
-         }
-        }
+        alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+      } else {
+        alert(
+          "Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor."
+        );
+      }
     }
+  };
 }
 
 function Kompis1() {
-    bilDialog = `
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "hilse()">👋</button>
             <button onclick = "hilse()">👊</button>
@@ -120,15 +119,15 @@ function Kompis1() {
         </div>`;
   const hilsninger = ["high five", "fist bump", "nikk"];
   this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
-  
+
   if (kulhetsFaktor <= 30) {
-    updateImg ="IMG/carkompis2.png";
+    updateImg = "IMG/carkompis2.png";
   } else if (kulhetsFaktor >= 30 && kulhetsFaktor <= 60) {
-    updateImg ="IMG/carkompis2upgrade1.png";
+    updateImg = "IMG/carkompis2upgrade1.png";
   } else if (kulhetsFaktor >= 60 && kulhetsFaktor <= 100) {
     updateImg = "IMG/carkompis2upgrade2.png";
   }
-  
+
   this.hils = function (hilsen) {
     if (hilsen === this.riktigHilsen) {
       if (kulhetsFaktor > 60) {
@@ -142,18 +141,18 @@ function Kompis1() {
       alert("Du hilste feil, og bilen din er ikke kul!");
       kulhetsFaktor -= 10;
       if (kulhetsFaktor < 70) {
-            alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-        } else {
-            alert(
-             "Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor."
-            );
-         }
-        }
+        alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+      } else {
+        alert(
+          "Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor."
+        );
+      }
     }
+  };
 }
 
 function Kompis2() {
-    bilDialog = `
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "hilse()">👋</button>
             <button onclick = "hilse()">👊</button>
@@ -161,15 +160,15 @@ function Kompis2() {
         </div>`;
   const hilsninger = ["high five", "fist bump", "nikk"];
   this.riktigHilsen = hilsninger[Math.floor(Math.random() * hilsninger.length)];
-  
+
   if (kulhetsFaktor <= 30) {
-    updateImg ="IMG/carkompis3.png";
+    updateImg = "IMG/carkompis3.png";
   } else if (kulhetsFaktor >= 30 && kulhetsFaktor <= 60) {
-    updateImg ="IMG/carkompis3upgrade1.png";
+    updateImg = "IMG/carkompis3upgrade1.png";
   } else if (kulhetsFaktor >= 60 && kulhetsFaktor <= 100) {
     updateImg = "IMG/carkompis3upgrade2.png";
   }
-  
+
   this.hils = function (hilsen) {
     if (hilsen === this.riktigHilsen) {
       if (kulhetsFaktor > 60) {
@@ -183,78 +182,93 @@ function Kompis2() {
       alert("Du hilste feil, og bilen din er ikke kul!");
       kulhetsFaktor -= 10;
       if (kulhetsFaktor < 70) {
-            alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-        } else {
-            alert(
-             "Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor."
-            );
-         }
-        }
+        alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+      } else {
+        alert(
+          "Du fikk en kald skulder av kompisen din, det svekket bilens kulhetsFaktor."
+        );
+      }
     }
+  };
 }
 
 function jehovasVitne() {
-    bilDialog = `
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "hilse()">👋</button>
             <button onclick = "hilse()">👊</button>
             <button onclick = "hilse()">🖕</button>
         </div>`;
-    kompisDialog = `
+  kompisDialog = `
     <div class="kompisDialog"></div>`;
 }
 
 function bestemor() {
-    bilDialog = `
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "hilse()">👋</button>
             <button onclick = "hilse()">👊</button>
             <button onclick = "hilse()">🖕</button>
         </div>`;
-    kompisDialog = `
+  kompisDialog = `
     <div class="kompisDialog"></div>`;
 }
 
 function flatEarther() {
-    bilDialog = `
+  bilDialog = `
         <div class="bilDialog"> 
             <button onclick = "hilse()">👋</button>
             <button onclick = "hilse()">👊</button>
             <button onclick = "hilse()">🖕</button>
         </div>`;
-    kompisDialog = `
+  kompisDialog = `
     <div class="kompisDialog"></div>`;
 }
 
-function upgrade() {
-
-}
-
+function upgrade() {}
 
 function hilse(hilsen) {
-    if (hilsen === this.riktigHilsen) {
-        if (kulhetsFaktor > 70) {
-            kulhetsFaktor += 10;
-            } else {
-                alert("Greit nok, men du kan bli mye kulere!");
-            }
-    }   else {
-            alert("Du hilste feil, og bilen din er ikke kul!");
-            kulhetsFaktor -= 10;
-            if (kulhetsFaktor < 70) {
-                alert("Du er fortsatt kul, men dette ble en klein situasjon.");
-            } else {
-                alert("Du fikk en kald skulder av kompisen din, det svekket bilens kulhet.");
-            }
-        }  
-    updateView();
+  if (hilsen === this.riktigHilsen) {
+    if (kulhetsFaktor > 70) {
+      kulhetsFaktor += 10;
+    } else {
+      alert("Greit nok, men du kan bli mye kulere!");
+    }
+  } else {
+    alert("Du hilste feil, og bilen din er ikke kul!");
+    kulhetsFaktor -= 10;
+    if (kulhetsFaktor < 70) {
+      alert("Du er fortsatt kul, men dette ble en klein situasjon.");
+    } else {
+      alert(
+        "Du fikk en kald skulder av kompisen din, det svekket bilens kulhet."
+      );
+    }
+  }
+  updateView();
 }
 
 function randomEvent() {
-
-}
+    let randomEncounter = Math.floor(Math.random() * 5) + 1;
+  
+    if (randomEncounter == 1) {
+      kompis();
+    } else if (randomEncounter == 2) {
+      kompis1();
+    } else if (randomEncounter == 3) {
+      kompis2();
+    } else if (randomEncounter == 4) {
+      jehovasVitne();
+    } else if(randomEncounter == 5){
+      flatEarther();
+    }
+    else {
+      bestemor();
+    }
+    
+  }
 
 function skjulHilseValg() {
-    document.getElementById("ukjent").style.display = "none";
-    document.getElementById("ukjent").style.display = "block";
+  document.getElementById("ukjent").style.display = "none";
+  document.getElementById("ukjent").style.display = "block";
 }
